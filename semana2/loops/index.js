a6790a2ab757673d04047a83bfa0586dca951f39
cleitonsoares-s <cleiton.ugo@hotmail.com>
2021-06-23 16:26:00 -0300

    Exercicio de loops

---
+++ semana2/loops/index.js
@@ -0,0 +1,106 @@
+// Exercícios de interpretação de código
+//1.
+// Soma a variavel valor com o valor de i, que sempre cresce +1 a cada rodada enquanto ele for menor que 5. Em seguida, imprime o valor de valor no console.
+
+//2.
+//a)
+// 19
+// 21
+// 23
+// 25
+// 27
+// 30
+
+// b)
+// ele é suficiente, é só tirar a condicional de números maiores que 18 que ele acessaria todos else.
+
+// 3.
+// *
+// **
+// ***
+//calculei errado e achei que ele só ia até o 3
+
+
+// Exercícios de escrita de código
+// 1.
+
+// let bichosEstimacao = Number(prompt("Quantos bichos de estimação você tem?"))
+
+// const arrBichinhos = []
+
+// if (bichosEstimacao === 0) {
+//   console.log("Que pena! Você pode adotar um pet!")
+// } else if (bichosEstimacao > 0) {
+//   let i = 0
+//   while (i < bichosEstimacao) {
+//     const bicho = prompt('Digite o nome de cada um deles')
+//     arrBichinhos.push(bicho)
+//     i++
+//   }
+//   console.log(arrBichinhos)
+// }
+
+
+// 2.
+
+const arrOriginal = [1, 2, 4, 6, 8, 9, 10, 55, 57]
+
+
+// // a)
+// const imprimeElementosArray = (array) => {
+//   for (const elemento of array) {
+//     console.log(elemento)
+//   }
+// }
+// imprimeElementosArray(arrOriginal)
+
+// // b)
+// const imprimeElementosDivididosPor10 = (array) => {
+//   for (const elemento of array) {
+//     console.log(elemento / 10)
+//   }
+// }
+// imprimeElementosDivididosPor10(arrOriginal)
+
+// // c)
+// const imprimeArrayDePares = (array) => {
+//   const arrayDePares = []
+//   for (const elemento of array) {
+//     if ((elemento % 2) === 0) {
+//       arrayDePares.push(elemento)
+//     }
+//   }
+//   console.log(arrayDePares)
+// }
+
+// imprimeArrayDePares(arrOriginal)
+
+
+// // d)
+// const retornaArrayString = (array) => {
+//   const novoArray = []
+//   for (const elemento of array) {
+//     novoArray.push(`O elemento do índex ${array.indexOf(elemento)} é ${elemento}`)
+//   }
+//   return novoArray
+// }
+
+// console.log(retornaArrayString(arrOriginal))
+
+// e)
+const imprimeMaiorEMenor = (array) => {
+  let valorMaximo = 0;
+  let valorMinimo;
+
+  let numeroAtual;
+  for (const i of array) {
+    numeroAtual = i
+
+    if (numeroAtual < i) {
+      valorMinimo = i
+    }
+  }
+  console.log(valorMinimo)
+}
+
+imprimeMaiorEMenor(arrOriginal)
