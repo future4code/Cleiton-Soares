6bc47d869d8e4239a6de9ad73eeb8c72ededd7e9
cleitonsoares-s <cleiton.ugo@hotmail.com>
2021-07-30 16:05:50 -0300

    ainda n consegui fazer o que queria

--- semana7/labefy/src/App.js
+++ semana7/labefy/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Playlists from './components/Playlists/Playlists';
+import { GlobalStyle } from './styles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+  criaPlaylist = () => {};
+
+  render() {
+    return (
+      <>
+        <Playlists/>
+        <GlobalStyle />
+      </>
+    );
+  }
 }
-
-export default App;
